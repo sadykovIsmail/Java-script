f235da281663398e764098085524e319e92b7e3b
Ismail <ghvh8176@gmail.com>
2025-05-04 20:54:03 +0600

    clear bugs

--- 36th project. FCC Authors page/script.js
+++ 36th project. FCC Authors page/script.js
@@ -13,6 +13,7 @@ fetch('https://cdn.freecodecamp.org/curriculum/news-author-page/authors.json')
   })
   .catch((err) => {
     console.error(`There was an error: ${err}`);
+
   });
 
 const fetchMoreAuthors = () => {
@@ -20,7 +21,10 @@ const fetchMoreAuthors = () => {
   endingIndex += 8;
 
   displayAuthors(authorDataArr.slice(startingIndex, endingIndex));
-
+  if (authorDataArr.length <= endingIndex) {
+    loadMoreBtn.disabled = true;
+    loadMoreBtn.textContent = 'No more data to load';
+  }
 };
 
 const displayAuthors = (authors) => {
@@ -29,8 +33,9 @@ const displayAuthors = (authors) => {
     <div id="${index}" class="user-card">
       <h2 class="author-name">${author}</h2>
       <img class="user-img" src="${image}" alt="${author} avatar" />
-      <p class="bio">${bio}</p>
-      <a class="author-link" href="${url}" target="_blank">${author}'s author page</a>
+      <div class="purple-divider"></div>
+      <p class="bio">${bio.length > 50 ? bio.slice(0, 50) + '...' : bio}</p>
+      <a class="author-link" href="${url}" target="_blank">${author} author page</a>
     </div>
   `;
   });
