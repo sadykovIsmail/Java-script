2ad60f274be879357aca3fddf5be59b6371b8ded
Ismail <ghvh8176@gmail.com>
2025-05-04 20:13:06 +0600

    36th: FCC Authors Page. Cards name

---
+++ 36th project. FCC Authors page/script.js
@@ -0,0 +1,26 @@
+const authorContainer = document.getElementById('author-container');
+const loadMoreBtn = document.getElementById('load-more-btn');
+
+let startingIndex = 0;
+let endingIndex = 8;
+let authorDataArr = [];
+
+fetch('https://cdn.freecodecamp.org/curriculum/news-author-page/authors.json')
+  .then((res) => res.json())
+  .then((data) => {
+    authorDataArr = data;
+    displayAuthors(authorDataArr.slice(startingIndex, endingIndex));
+  })
+  .catch((err) => {
+    console.error(`There was an error: ${err}`);
+  });
+
+const displayAuthors = (authors) => {
+  authors.forEach(({ author, image, url, bio }, index) => {
+    authorContainer.innerHTML += `
+    <div id="${index}" class="user-card">
+      <h2 class="author-name">${author}</h2>
+    </div>
+  `;
+  });
+};
